862debaff7fc8035dab1bf821af4e3c61c4f334e
holczur <holcz.norbert@gmail.com>
2021-03-19 07:12:06 +0100

    practice exercise

--- Data_structures_operators/Strings/script.js
+++ Data_structures_operators/Strings/script.js
@@ -80,3 +80,21 @@ const planesInLine = function (n) {
 planesInLine(3);
 planesInLine(5);
 planesInLine(10);
+
+//PRACTICE
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type
+    .replaceAll('_', ' ')
+    .trim()} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45);
+  console.log(output);
+}
