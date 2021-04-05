6c4739f15c1fc7e5342480dc6bd210ae96af4f03
holczur <holcz.norbert@gmail.com>
2021-04-05 13:31:51 +0200

    implementing CLOSE ACC

--- Section 11 - Working with arrays/BANKIST APP/script.js
+++ Section 11 - Working with arrays/BANKIST APP/script.js
@@ -164,3 +164,19 @@ btnTransfer.addEventListener('click', function (e) {
     inputTransferAmount.blur();
   }
 });
+
+//Event handler - CLOSE
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = 'Log in to get started';
+  }
+});
