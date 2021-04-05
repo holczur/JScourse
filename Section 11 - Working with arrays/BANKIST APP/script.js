4e2b48d4163187b55f00672df47c54c4e9e193b6
holczur <holcz.norbert@gmail.com>
2021-04-05 11:14:30 +0200

    calcDisplaySummary() is ready

--- Section 11 - Working with arrays/BANKIST APP/script.js
+++ Section 11 - Working with arrays/BANKIST APP/script.js
@@ -66,7 +66,7 @@ const displayMovement = function (movements) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}</div>
+        <div class="movements__value">${mov} €</div>
       </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
@@ -75,7 +75,7 @@ displayMovement(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance} €`;
 };
 
 calcDisplayBalance(account1.movements);
@@ -90,5 +90,28 @@ const createUserNames = function (accs) {
       .join(''); // merge them
   });
 };
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 0.08) / 100) //interest rate
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1; //only paid if amount is >= 1€
+    })
+    .reduce((acc, dep) => acc + dep, 0);
+  labelSumInterest.textContent = `${interest} €`;
+};
+calcDisplaySummary(account1.movements);
 
 createUserNames(accounts);
