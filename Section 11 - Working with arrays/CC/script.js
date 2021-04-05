589ccb93a49c1e5e3e15f5837b221ad6fd86698b
holczur <holcz.norbert@gmail.com>
2021-04-05 11:25:33 +0200

    refine avg calc method on S11CC3

--- Section 11 - Working with arrays/CC/script.js
+++ Section 11 - Working with arrays/CC/script.js
@@ -85,12 +85,10 @@ Test data:
 GOOD LUCK �
 */
 
-const calcAverageHumanAgeArrow = ages => {
+const calcAverageHumanAgeArrow = ages =>
   ages
     .map(age => (age <= 2 ? age * 2 : age * 4 + 16))
     .filter(age => age >= 18)
-    .reduce((acc, age) => acc + age, 0) / ages.length;
-};
-
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 const avgArrow1 = calcAverageHumanAgeArrow([5, 2, 4, 1, 15, 8, 3]);
 const avgArrow2 = calcAverageHumanAgeArrow([16, 6, 10, 5, 6, 1, 4]);
