c801622c983c5ba6ab36ffde42e1b1422be22abd
holczur <holcz.norbert@gmail.com>
2021-04-06 09:54:32 +0200

    LOAN functionality is ready

--- Section 11 - Working with arrays/BANKIST APP/script.js
+++ Section 11 - Working with arrays/BANKIST APP/script.js
@@ -180,3 +180,14 @@ btnClose.addEventListener('click', function (e) {
     labelWelcome.textContent = 'Log in to get started';
   }
 });
+
+//Event handler - LOAN
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+  }
+  updateUI(currentAccount);
+  inputLoanAmount.value = '';
+});
