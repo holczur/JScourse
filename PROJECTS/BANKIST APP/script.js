caccf9bc0493aed304fe20d81eeb74800ac1536e
holczur <holcz.norbert@gmail.com>
2021-04-19 07:53:22 +0200

    apply rounding to loan amount

--- PROJECTS/BANKIST APP/script.js
+++ PROJECTS/BANKIST APP/script.js
@@ -198,7 +198,7 @@ btnClose.addEventListener('click', function (e) {
 //Event handler - LOAN
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     currentAccount.movements.push(amount);
   }
