0ab9d0c6068d67c8ba00febb8cb07a3010709528
holczur <holcz.norbert@gmail.com>
2021-04-19 11:10:48 +0200

    s12 - Dates

--- Section 12 - Numbers-Dates-Intl-Timers/script.js
+++ Section 12 - Numbers-Dates-Intl-Timers/script.js
@@ -1,5 +1,47 @@
 'use strict';
 
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const accounts = [account1, account2];
+
 //Numbers are allways stores in binary form
 //In binary there are some circumstances that give us weried results
 //That's because with base 2 some fregmentation is infinite, just like in base 10 (3/10 = 3.33333...)
@@ -127,3 +169,33 @@ console.log(20 == 20n); // true - type coercion
 console.log(10n / 3n); //3n -> decimal part is cut off
 
 console.log('------------DATES AND TIMES---------------');
+const now = new Date();
+console.log(now);
+
+console.log(account1.movementsDates[0]);
+console.log(new Date(2037, 0, 19, 15, 23, 5)); //months are zero based
+console.log(new Date(2037, 10, 31)); //autocorrect non existing dates 11.31 -> 12.01
+console.log(new Date(2037, 10, 32)); //autocorrect non existing dates 11.32 -> 12.02
+
+console.log(new Date(0)); // Unix time: January 1, 1970
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); // Unix time + 3 days (add in milliseconds)
+console.log(3 * 24 * 60 * 60 * 1000); // Timestamp of day three
+
+//Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future.getFullYear());
+console.log(future.getMonth()); // ZERO BASED!!! EG.: 10 MEANS NOVEMBER
+console.log(future.getDate()); // WHICH DAY OF MONTH
+console.log(future.getDay()); // WHICH DAY OF WEEK (0 = SUNDAY)
+console.log(future.getHours()); // WHICH DAY OF WEEK (0 = SUNDAY)
+console.log(future.getMinutes()); // WHICH DAY OF WEEK (0 = SUNDAY)
+console.log(future.getSeconds()); // WHICH DAY OF WEEK (0 = SUNDAY)
+
+console.log(future.toISOString()); //international standard
+console.log(future.getTime()); // // timestamp (How many millisecond had passed since 01.01.1970.)
+console.log(new Date(2142253380000)); // timestamp can be used to create new date
+
+console.log(Date.now()); // timestamp of this moment
+
+future.setFullYear(2040); // all date properties can be set individually
+console.log(future);
