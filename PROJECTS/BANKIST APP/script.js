3d74a3558f5dcfd27a0feac1dca0dcf30011c75d
holczur <holcz.norbert@gmail.com>
2021-04-20 07:17:45 +0200

    Change movements date display (Today\Yesterday\2-7 Days ago\Date)

--- PROJECTS/BANKIST APP/script.js
+++ PROJECTS/BANKIST APP/script.js
@@ -8,13 +8,13 @@ const account1 = {
 
   movementsDates: [
     '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-04-13T07:42:02.383Z',
+    '2021-04-14T09:15:04.904Z',
+    '2021-04-15T10:17:24.185Z',
+    '2021-04-16T14:11:59.604Z',
+    '2021-04-17T17:01:17.194Z',
+    '2021-04-18T21:36:17.929Z',
+    '2021-04-19T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -70,6 +70,14 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 //
 const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
   const day = `${date.getDate()}`.padStart(2, 0);
   const month = `${date.getMonth() + 1}`.padStart(2, 0);
   const year = date.getFullYear();
